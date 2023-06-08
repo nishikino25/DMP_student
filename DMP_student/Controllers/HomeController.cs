8b30a42a0c8a2f690cc3453f87fc73159baa9a26
nishikino25 <cute01jenny@gmail.com>
2023-06-08 17:26:02 +0800

    add css js folder

--- DMP_student/Controllers/HomeController.cs
+++ DMP_student/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using DMP_student.Models;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Json;
 
 namespace DMP_student.Controllers
 {
@@ -28,5 +30,52 @@ namespace DMP_student.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        /*public ActionResult Upload(List<HttpPostedFileBase> files) //單一檔案參數 HttpPostedFileBase file
+        {
+            //單一檔案
+            //if (file != null)
+            //{
+            //    if (file.ContentLength > 0)
+            //    {
+            //        string path = Path.Combine(Server.MapPath("~/FileUploads/"));
+            //        if (!Directory.Exists(path))
+            //        {
+            //            Directory.CreateDirectory(path);
+            //        }
+
+            //        var fileName = Path.GetFileName(file.FileName);
+            //        file.SaveAs(path + fileName);
+            //    }
+            //}
+
+            string message = "";
+
+            //多檔案
+            if (files != null)
+            {
+                if (files.Count > 0)
+                {
+                    //path 看要不要再串接 學號 + 單號
+                    string path = Path.Combine(Server.MapPath("~/FileUploads/"));
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    foreach (HttpPostedFileBase file in files)
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        file.SaveAs(path + fileName);
+                    }
+                }
+            }
+
+            string serverMessage = JsonContent.SerializeObject(message);
+            return Json(new { serverMessage }, JsonRequestBehavior.AllowGet);
+        }
+        */
+
     }
+
 }
